99e8f97e77030a640a1f12e0fcbd6e0647f6afee
ro <rodriseguramo@gmail.com>
2020-12-13 17:21:08 -0600

    Changed files

--- Tareas/T4.1_Programa/MiPrograma.py
+++ Tareas/T4.1_Programa/MiPrograma.py
@@ -1,41 +1,52 @@
-print("Bienvenidx. Este script te permitirá calcular las raíces de una función cuadrática.")
+print("Bienvenidx. Este script te permitirá calcular el volumen de tres polígonos platónicos.")
 
 while True:
-    print("Si deseas calcular las raíces introduce [1] si deseas salir introduce [2]")
+    print("""Teclea el número del sólido que deseas calcular el volumen: Tetraedro [1], Octaedro  [2], Icosaedro [3]
+    
+    O bien, teclea [4] para salir del programa.""")
+
     option = input("> ")
 
+    # Tetraedro
     if option == str(1):
         while True:
             try:
-                a = float(input("Valor de a: "))
-                while True:
-                    try:
-                        b = float(input("Valor de b: "))
-                        while True:
-                            try:
-                                c = float(input("Valor de c: "))
-                                break
-                            except:
-                                print("Por favor, introduce un número válido para 'c'.")
-                        break
-                    except:
-                        print("Por favor introduce un número válido para 'b'.")
+                l = float(input("Valor de la arista: "))
                 break
             except ValueError:
-                print("Por favor introduce un número válido para 'a'.")
+                print("Por favor introduce una longitud válida para el arista.")
+        v = ((2**(1/2))*(l)**3)/(12)
+        print("El volumen de un tetraedro regular con aristas de longitud " + str(l) + " es " + str(v) + " u³" + """
         
-        x_1 = (-b + (b**2-4*a*c)**(1/2) )/(2*a)
-        x_2 = (-b - (b**2-4*a*c)**(1/2) )/(2*a)
+        """)
 
-        valor_a = "x²" if a==1 else "-x²" if a==-1 else str(a) + "x²"
-
-        valor_b = "x" if b==1 else "-x" if b==-1 else "+" + str(b) + "x" if b>0 else str(b) + "x"
-
-        valor_c = "" if c==0 else "+" + str(c) if c>0 else str(c)
-
-        print("Las soluciones a la ecuación " + valor_a + valor_b + valor_c + " son: " + str(x_1) + " y " + str(x_2))
-
+    # Octaedro
     elif option == str(2):
+        while True:
+            try:
+                l = float(input("Valor de la arista: "))
+                break
+            except ValueError:
+                print("Por favor introduce una longitud válida para el arista.")
+        v = ((2**(1/2))*(l)**3)/(3)
+        print("El volumen de un octaedro regular con aristas de longitud " + str(l) + " es " + str(v) + " u³" + """
+        
+        """)
+
+    # Icosaedro
+    elif option == str(3):
+        while True:
+            try:
+                l = float(input("Valor de la arista: "))
+                break
+            except ValueError:
+                print("Por favor introduce una longitud válida para el arista.")
+        v = ((5)/(12))*(3+(5)**(1/2))*(l**3)
+        print("El volumen de un icosaedro regular con aristas de longitud " + str(l) + " es " + str(v) + " u³" + """ 
+        
+        """)
+
+    elif option == str(4):
         break
 
     else:
